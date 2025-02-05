8466a3205e98df8daf9ca7b23032562e0ad2b268
Edgar Pérez <edgarperezperez554@gmail.com>
2025-02-05 12:34:12 -0600

    version 0.001

--- 2OLIDTS-EdgarPerez-04py/_2OLIDTS_EdgarPerez_04py.py
+++ 2OLIDTS-EdgarPerez-04py/_2OLIDTS_EdgarPerez_04py.py
@@ -1,11 +1,13 @@
 maxf=3
-maxc=5
-a=[[0.0]*maxc for _ in range (maxf)]
+maxc=3
+a=[[0.0]*maxc for _ in range (maxf)] #creacion de matrix de ceros (Arreglo bidimencional)
 #leer el arrary
 for f in range (maxf):
     for c in range (maxc):
-        a[f][c]=float (input(f'ingrese los elementos de la matriz: '))
+        print ("Ingrese un valor: ")
+        a[f][c]=float (input())
 #escribir el array
+print ("Impresion de Balores almacenados")
 for f in range (maxf):
     for c in range (maxc):
         print(a[f][c], end=" ")
